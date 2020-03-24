4aeb9980652a83f2e85b25d713e760f498205e64
William Liu <superliuxz@gmail.com>
2020-03-24 15:04:56 -0700

    wth???

--- G-DBSCAN/gpu/gdbscan.cuh
+++ G-DBSCAN/gpu/gdbscan.cuh
@@ -9,10 +9,21 @@
 #include <thrust/execution_policy.h>
 #include <thrust/scan.h>
 
+#define CUDA_ERR_CHK(ans) \
+  { cuda_err_chk((ans), __FILE__, __LINE__); }
+inline void cuda_err_chk(cudaError_t code, const char *file, int line,
+                         bool abort = true) {
+  if (code != cudaSuccess) {
+    fprintf(stderr, "CUDA_ERR_CHK: %s %s %d\n", cudaGetErrorString(code), file,
+            line);
+    if (abort) exit(code);
+  }
+}
+
 // TODO:
 // - construct Va and Ea, DO NOT use temp_adj. AVOID data transfer to GPU.
 //   - [x] Va: thrust::exclusive_scan
-//   - Ea: one thread each node
+//   - [x] Ea: one thread each node
 // - ID core and non-core: one thread each node
 // - BFS. no clue ... on thread each node? then ima hving a n^2 bfs???
 //
@@ -24,47 +35,103 @@ __device__ float square_dist(const float &x1, const float &y1, const float &x2,
   return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
 }
 
-// Kernel to populate Va
-__global__ void pop_va(float const *const x, float const *const y,
-                       uint64_t *const Va, const float rad_sq,
-                       const uint64_t num_nodes) {
+// Kernel to calculate the number of neighbours of each node
+__global__ void k_num_nbs(float const *const x, float const *const y,
+                          uint64_t *const num_nbs, const float rad_sq,
+                          const uint64_t num_nodes) {
   uint64_t const u = threadIdx.x + blockIdx.x * blockDim.x;
   if (u >= num_nodes) return;
-  uint64_t num_nbs = 0;
+  num_nbs[u] = 0;
   for (auto v = 0u; v < num_nodes; ++v) {
-    if (u != v && square_dist(x[u], y[u], x[v], y[v]) <= rad_sq) ++num_nbs;
+    if (u != v && square_dist(x[u], y[u], x[v], y[v]) <= rad_sq) ++num_nbs[u];
   }
-  Va[u] = num_nbs;
 }
 
-void insert_edge(float const *const x, float const *const y, uint64_t *const Va,
-                 const float &rad_sq, const uint64_t &num_nodes) {
-  const auto coord_size = sizeof(x[0]) * num_nodes;
-  const auto Va_size = sizeof(Va[0]) * num_nodes;
+void calc_num_neighbours(float const *const x, float const *const y,
+                         uint64_t *const num_nbs, const float &rad_sq,
+                         const uint64_t &num_nodes) {
+  const auto N = sizeof(x[0]) * num_nodes;
+  const auto K = sizeof(num_nbs[0]) * num_nodes;
+
+  printf("calc_num_neighbours needs: %lf MB\n",
+         static_cast<double>(N + N + K) / 1024.f / 1024.f);
 
   const auto num_blocks = std::ceil(num_nodes / static_cast<float>(blocksize));
   float *dev_x, *dev_y;
-  uint64_t *dev_Va;
-  cudaMalloc((void **)&dev_x, coord_size);
-  cudaMalloc((void **)&dev_y, coord_size);
-  cudaMalloc((void **)&dev_Va, Va_size);
-  cudaMemcpy(dev_x, x, coord_size, cudaMemcpyHostToDevice);
-  cudaMemcpy(dev_y, y, coord_size, cudaMemcpyHostToDevice);
+  uint64_t *dev_num_nbs;
+  CUDA_ERR_CHK(cudaMalloc((void **)&dev_x, N));
+  CUDA_ERR_CHK(cudaMalloc((void **)&dev_y, N));
+  CUDA_ERR_CHK(cudaMalloc((void **)&dev_num_nbs, K));
+  CUDA_ERR_CHK(cudaMemcpy(dev_x, x, N, cudaMemcpyHostToDevice));
+  CUDA_ERR_CHK(cudaMemcpy(dev_y, y, N, cudaMemcpyHostToDevice));
 
-  pop_va<<<num_blocks, blocksize>>>(dev_x, dev_y, dev_Va, rad_sq, num_nodes);
+  k_num_nbs<<<num_blocks, blocksize>>>(dev_x, dev_y, dev_num_nbs, rad_sq,
+                                       num_nodes);
 
-  cudaDeviceSynchronize();
-
-  cudaMemcpy(Va, dev_Va, Va_size, cudaMemcpyDeviceToHost);
-  cudaFree(dev_x);
-  cudaFree(dev_y);
-  cudaFree(dev_Va);
+  CUDA_ERR_CHK(cudaMemcpy(num_nbs, dev_num_nbs, K, cudaMemcpyDeviceToHost));
+  CUDA_ERR_CHK(cudaFree(dev_x));
+  CUDA_ERR_CHK(cudaFree(dev_y));
+  CUDA_ERR_CHK(cudaFree(dev_num_nbs));
 }
 
-void calc_Va(uint64_t *const Va, const uint64_t num_nodes) {
-  thrust::exclusive_scan(thrust::host, Va, Va + num_nodes, Va);
+void calc_start_pos(uint64_t const *const num_nbs, uint64_t *const start_pos,
+                    const uint64_t num_nodes) {
+  thrust::exclusive_scan(thrust::host, num_nbs, num_nbs + num_nodes, start_pos);
 }
 
+// Kernel to populate the actual neighbours array
+__global__ void k_append_neighbours(float const *const x, float const *const y,
+                                    uint64_t const *const start_pos,
+                                    uint64_t *const neighbours,
+                                    const uint64_t num_nodes,
+                                    const float rad_sq) {
+  uint64_t const u = threadIdx.x + blockIdx.x * blockDim.x;
+  if (u >= num_nodes) return;
+  uint64_t upos = start_pos[u];
+  for (uint64_t v = 0u; v < num_nodes; ++v) {
+    if (u != v && square_dist(x[u], y[u], x[v], y[v]) <= rad_sq) {
+      neighbours[upos++] = v;
+    }
+  }
+}
+
+void append_neighbours(float const *const x, float const *const y,
+                       uint64_t const *const start_pos,
+                       uint64_t *const neighbours, const uint64_t num_nodes,
+                       uint64_t const nb_arr_sz, float const rad_sq) {
+  std::cout << "append_neighbours called" << std::endl;
+  const auto num_blocks = std::ceil(num_nodes / static_cast<float>(blocksize));
+  std::cout << "num_blocks calculated" << std::endl;
+
+  const auto N = sizeof(x[0]) * num_nodes;
+  const auto K = sizeof(start_pos[0]) * num_nodes;
+  const auto J = sizeof(neighbours[0]) * nb_arr_sz;
+
+  printf("append_neighbours needs: %lf MB\n",
+         static_cast<double>(N + N + K + J) / 1024.f / 1024.f);
+
+  float *dev_x, *dev_y;
+  uint64_t *dev_start_pos, *dev_neighbours;
+
+  CUDA_ERR_CHK(cudaMalloc((void **)&dev_x, N));
+  CUDA_ERR_CHK(cudaMalloc((void **)&dev_y, N));
+  CUDA_ERR_CHK(cudaMalloc((void **)&dev_start_pos, K));
+  CUDA_ERR_CHK(cudaMalloc((void **)&dev_neighbours, J));
+
+  CUDA_ERR_CHK(cudaMemcpy(dev_x, x, N, cudaMemcpyHostToDevice));
+  CUDA_ERR_CHK(cudaMemcpy(dev_y, y, N, cudaMemcpyHostToDevice));
+  CUDA_ERR_CHK(cudaMemcpy(dev_start_pos, start_pos, K, cudaMemcpyHostToDevice));
+
+  k_append_neighbours<<<num_blocks, blocksize>>>(
+      dev_x, dev_y, dev_start_pos, dev_neighbours, num_nodes, rad_sq);
+
+  CUDA_ERR_CHK(
+      cudaMemcpy(neighbours, dev_neighbours, J, cudaMemcpyDeviceToHost));
+  CUDA_ERR_CHK(cudaFree(dev_x));
+  CUDA_ERR_CHK(cudaFree(dev_y));
+  CUDA_ERR_CHK(cudaFree(dev_start_pos));
+  CUDA_ERR_CHK(cudaFree(dev_neighbours));
+}
 }  // namespace GDBSCAN
 
 #endif  // DBSCAN_GDBSCAN_CUH
--- G-DBSCAN/gpu/main.cu
+++ G-DBSCAN/gpu/main.cu
@@ -6,15 +6,14 @@
 
 #include "gdbscan.cuh"
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   cxxopts::Options options("GDBSCAN", "ma, look, it's GDBSCAN");
   // clang-format off
     options.add_options()
             ("p,print", "Print clustering IDs") // boolean
             ("r,eps", "Clustering radius", cxxopts::value<float>())
             ("n,min-samples", "Number of points within radius", cxxopts::value<size_t>())
-            ("i,input", "Input filename", cxxopts::value<std::string>())
-            ;
+            ("i,input", "Input filename", cxxopts::value<std::string>());
   // clang-format on
   auto args = options.parse(argc, argv);
 
@@ -31,7 +30,7 @@ int main(int argc, char* argv[]) {
   ifs >> num_nodes;
 
   float xs[num_nodes], ys[num_nodes];
-  uint64_t Va[num_nodes];
+  uint64_t num_neighbours[num_nodes], start_pos[num_nodes];
 
   uint64_t n;
   float x, y;
@@ -40,13 +39,34 @@ int main(int argc, char* argv[]) {
     ys[n] = y;
   }
 
-  GDBSCAN::insert_edge(xs, ys, Va, radius * radius, num_nodes);
-  GDBSCAN::calc_Va(Va, num_nodes);
-
+  GDBSCAN::calc_num_neighbours(xs, ys, num_neighbours, radius * radius,
+                               num_nodes);
   if (output_labels) {
-    std::cout << "Va:" << std::endl;
+    std::cout << "num_neighbours:" << std::endl;
     for (auto i = 0u; i < num_nodes; ++i)
-      std::cout << i << " " << Va[i] << std::endl;
+      std::cout << i << " " << num_neighbours[i] << std::endl;
   }
+
+  GDBSCAN::calc_start_pos(num_neighbours, start_pos, num_nodes);
+  if (output_labels) {
+    std::cout << "start_pos:" << std::endl;
+    for (auto i = 0u; i < num_nodes; ++i)
+      std::cout << i << " " << start_pos[i] << std::endl;
+  }
+
+  const uint64_t nbarr_sz =
+      start_pos[num_nodes - 1] + num_neighbours[num_nodes - 1];
+  std::cout << "size of neighbours array: " << nbarr_sz << std::endl;
+  auto *neighbours = new uint64_t[nbarr_sz];
+//  uint64_t neighbours[nbarr_sz];
+  std::cout << "huhuhuhuh" << std::endl;
+  GDBSCAN::append_neighbours(xs, ys, start_pos, neighbours, num_nodes,
+                             nbarr_sz, radius * radius);
+  if (output_labels) {
+    std::cout << "neighbours:" << std::endl;
+    for (auto i = 0u; i < nbarr_sz; ++i)
+      std::cout << i << " " << neighbours[i] << std::endl;
+  }
+  delete []neighbours;
   return 0;
 }
