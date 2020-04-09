487b59d2150a131f31b3253d692a19c3bc582b92
William Liu <superliuxz@gmail.com>
2020-04-08 19:48:43 -0700

    add runtime tracker

--- gpu/main/main.cu
+++ gpu/main/main.cu
@@ -31,17 +31,39 @@ int main(int argc, char *argv[]) {
 
   GDBSCAN::Solver solver(input, min_pts, radius);
 
+  auto t0 = high_resolution_clock::now();
+  auto d0 = duration_cast<duration<double>>(t0 - start);
   solver.construct_grid();
+  printf("construct_grid takes %lf seconds\n", d0.count());
+
   solver.calc_num_neighbours();
+  auto t1 = high_resolution_clock::now();
+  auto d1 = duration_cast<duration<double>>(t1 - t0);
+  printf("calc_num_neighbours takes %lf seconds\n", d1.count());
+
   solver.calc_start_pos();
+  auto t2 = high_resolution_clock::now();
+  auto d2 = duration_cast<duration<double>>(t2 - t1);
+  printf("calc_start_pos takes %lf seconds\n", d2.count());
+
   solver.append_neighbours();
+  auto t3 = high_resolution_clock::now();
+  auto d3 = duration_cast<duration<double>>(t3 - t2);
+  printf("append_neighbours takes %lf seconds\n", d3.count());
+
   solver.identify_cores();
+  auto t4 = high_resolution_clock::now();
+  auto d4 = duration_cast<duration<double>>(t4 - t3);
+  printf("identify_cores takes %lf seconds\n", d4.count());
+
   solver.identify_clusters();
+  auto t5 = high_resolution_clock::now();
+  auto d5 = duration_cast<duration<double>>(t5 - t4);
+  printf("identify_clusters takes %lf seconds\n", d5.count());
 
   duration<double> time_spent =
       duration_cast<duration<double>>(high_resolution_clock::now() - start);
-  std::cout << "G-DBSCAN takes " << time_spent.count() << " seconds"
-            << std::endl;
+  printf("GDBSCAN takes %lf seconds\n", time_spent.count());
 
   if (output_labels) {
     std::cout << "cluster ids:" << std::endl;
